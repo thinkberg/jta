73baab8210f67222a017b7b96b18de75c621c7c2
leo <leo@03d0f0ad-74f4-0310-ae7a-cf78699b6f6c>
1999-11-05 22:25:40 +0000

    user url checking ...

--- bin/users.pl
+++ bin/users.pl
@@ -5,21 +5,26 @@ open(USERDB, "sort html/users.db|");
 while(<USERDB>) {
   chomp;
   ($service, $name, $url, $telnet, $description) = split /[|]/;
-  print STDERR "Checking $url ... ";
-  if(!system("wget --quiet --spider $url")) {
-    print STDERR "OK\n";
-    $OUT{$service} .= "<LI><A HREF=\"$url\">$name</A>";
-  } else {
-    print STDERR "DEFECT\n";
-    $OUT{$service} .= "<LI><A HREF=\"$url\">$name</A> [<I>defect?</I>] ";
+  if(length $url) {
+    print STDERR "Checking $url ... ";
+    if(length $url && !system("wget --timeout=10 --quiet --spider $url")) {
+      print STDERR "OK\n";
+      $OUT{$service} .= "<LI><A HREF=\"$url\">$name</A>";
+    } else {
+      print STDERR "DEFECT\n";
+      $OUT{$service} .= "<LI><A HREF=\"$url\">$name</A> [?] ";
+    }
   }
-  print STDERR "Checking $telnet ... ";
-  if(!system("wget --quiet --spider $telnet")) {
-    print STDERR "OK\n";
-    $OUT{$service} .= "(<I><A HREF=\"$telnet\">telnet</A></I>)\n";
-  } else {
-    print STDERR "DEFECT\n";
-    $OUT{$service}.="(<I><A HREF=\"$telnet\">telnet</A></I> [<I>defect?</I>])\n";
+  if(length $telnet) {
+    print STDERR "Checking $telnet ... ";
+    if(length $telnet &&!system("wget --timeout=10 --quiet --spider $telnet")) {
+      print STDERR "OK\n";
+      $OUT{$service} .= "(<I><A HREF=\"$telnet\">telnet</A></I>)\n";
+    } else {
+      print STDERR "DEFECT\n";
+      $OUT{$service} .=
+        "(<I><A HREF=\"$telnet\">telnet</A></I> [?])\n";
+    }
   }
 }
 close(USERDB);
