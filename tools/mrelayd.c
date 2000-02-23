cba2fc69e8eab2a8ef03e6aef54b46deb610a5eb
marcus <marcus@03d0f0ad-74f4-0310-ae7a-cf78699b6f6c>
2000-02-23 11:16:55 +0000

    removed bzero prototype , not used anyway

--- tools/mrelayd.c
+++ tools/mrelayd.c
@@ -87,7 +87,6 @@ int shutdown(int,int);
 int close(int);
 int bind(int,struct sockaddr*,int);
 int listen(int,int);
-void bzero(char*,int);
 int select(int,fd_set *,fd_set*,fd_set*,struct timeval*);
 int accept(int,struct sockaddr*,int*);
 int connect(int,struct sockaddr*,int);
@@ -99,10 +98,6 @@ int recvfrom(int,char*,int,int,struct sockaddr*,int*);
 #ifdef hpux
 /* redefinition... to avoid prototype in <time.h> */
 #define FD_CAST int
-#endif
-
-#ifdef sgi
-void bzero(void*,int);
 #endif
 
 #ifndef FD_CAST
