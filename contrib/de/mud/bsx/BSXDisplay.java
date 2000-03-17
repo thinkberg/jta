76fab7d51530de3d6f7c4994e44c90803ab3641b
thomas <thomas@03d0f0ad-74f4-0310-ae7a-cf78699b6f6c>
2000-03-17 13:16:54 +0000

    released under GPL as "may be part of The Java Telnet Application"

--- contrib/de/mud/bsx/BSXDisplay.java
+++ contrib/de/mud/bsx/BSXDisplay.java
@@ -1,7 +1,20 @@
-/**
- * Implementation of a BSX Display capable of handling basic BSX-Calls.
- * Handles: @SCE, @DFS, @VIO, @RMO, @DFO, @RFS
- * Uses:    BSXCache, BSXObject, BSXScene
+/*
+ * This file may be part of "The Java Telnet Application".
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * "The Java Telnet Application" is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 package de.mud.bsx;
 
@@ -15,6 +28,9 @@ import java.awt.Frame;
 import java.util.Enumeration;
 /**
  * the visual component for displaying bsx data.
+ * Implementation of a BSX Display capable of handling basic BSX-Calls.
+ * Handles: @SCE, @DFS, @VIO, @RMO, @DFO, @RFS
+ * Uses:    BSXCache, BSXObject, BSXScene
  @author  Thomas Kriegelstein
  @version 1.0
  */
